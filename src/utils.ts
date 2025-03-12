ffd16a5d542cd7ea1683b9926dbf676f81d398d0
Don Campbell <timebox.designs@gmail.com>
2025-03-12 13:17:38 -0600

    refactor: minor tweek

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,10 @@ import { constants } from '@/constants';
 // prettier-ignore
 export const eq = <T,>(a: T) => (b: T) => a === b;
 
+export const position = Vector.create;
+
+export const size = (width: number, height: number): Size => ({ width, height });
+
 export const getRandomInt = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -15,12 +19,8 @@ export const generatePairs = (offset = 0) => {
   const y = getRandomInt(0, constants.PipeHeight / 2);
   const gap = getRandomInt(280, 340);
 
-  const top = { position: { x, y } };
-  const bottom = { position: { x, y: y + gap + constants.MaxHeight - constants.PipeHeight } };
+  const top = { position: position(x, y) };
+  const bottom = { position: position(x, y + gap + constants.MaxHeight - constants.PipeHeight) };
 
-  return { top, bottom, size: { width: constants.PipeWidth, height: constants.PipeHeight } };
+  return { top, bottom, size: size(constants.PipeWidth, constants.PipeHeight) };
 };
-
-export const position = Vector.create;
-
-export const size = (width: number, height: number): Size => ({ width, height });
